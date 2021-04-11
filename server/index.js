102fefe33ec7abb96ce520e2569414c310e80bc1
Alexey G. Standalone <coder2k@mail.ru>
2021-04-11 11:31:17 +0300

    добавлен graphql к серверу

--- server/index.js
+++ server/index.js
@@ -1,21 +1,35 @@
 const express = require('express')
 const next = require('next')
+const {graphqlHTTP} = require("express-graphql");
+const {buildSchema} = require("graphql");
+
 
 const port = parseInt(process.env.PORT, 10) || 3000
 const dev = process.env.NODE_ENV !== 'production'
-const app = next({ dev })
+const app = next({dev})
 const handle = app.getRequestHandler()
+
 
 app.prepare().then(() => {
     const server = express()
 
-    // server.get('/a', (req, res) => {
-    //     return app.render(req, res, '/a', req.query)
-    // })
-    //
-    // server.get('/b', (req, res) => {
-    //     return app.render(req, res, '/b', req.query)
-    // })
+    // Подключаем graphql к нашему серверу
+    const schema = buildSchema(`
+        type Query {
+        hello: String
+        }
+    `)
+
+    // Resolver для наших запросов
+    const root = {
+        hello: () => 'Hello from resolver!'
+    }
+
+    server.use('/graphql', graphqlHTTP({
+        schema,
+        rootValue: root,
+        graphiql: true  // отображает "песочицу"
+    }))
 
     server.all('*', (req, res) => {
         return handle(req, res)
